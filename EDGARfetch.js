ebb16400d94ca88e993108d748dfa11d2cf688f0
yaniv <yaniv@yanivalfasy.com>
2020-04-05 17:29:32 -0700

    updated query database

--- EDGARfetch.js
+++ EDGARfetch.js
@@ -4,8 +4,9 @@
 // DECLARE THE REQUREMENT FILES 
 let EDGAR_fetch = require('./fetch.js')
 let EDGAR_store = require('./store.js')
+let EDGAR_query = require('./query.js')
 
-// DECLARE ALL THE DIRECTORIES
+// DECLARE ALL THE VARIABLES
 
 	var EDGAR_archive_url = 'https://www.sec.gov/Archives/edgar/full-index/'
 	const quarters = ['QTR1','QTR2','QTR3','QTR4']
@@ -14,7 +15,7 @@ let EDGAR_store = require('./store.js')
 	var years = Array.from(new Array(endyear-startyear+1), (x,i) => i + startyear)
 
 
-// GET ALL THE INDEXES
+// GET ALL THE INDEXES //EXAMPLE getIndexes(years,quarters)
 function getIndexes(years,quarters){
 
 	years.forEach(year=>
@@ -29,7 +30,7 @@ function getIndexes(years,quarters){
 	)
 }
 
-// UNTAR ALL THE INDEXES
+// UNTAR ALL THE INDEXES //EXAMPLE unzip_Indexes(years,quarters)
 function unzip_Indexes(years,quarters){
 	years.forEach(year=>
 		quarters.forEach(function(qtr){
@@ -41,7 +42,7 @@ function unzip_Indexes(years,quarters){
 	)
 }
 
-// STORE ALL THE FILES
+// STORE ALL THE FILES //EXAMPLE store_Indexes(years,quarters)
 async function store_Indexes(years,quarters) {
 
 	// CREATE ARRAY OF FILES
@@ -61,5 +62,12 @@ async function store_Indexes(years,quarters) {
 	  console.log('Done!');
 }
 
+// QUERY THE DATBASE //EXAMPLE queryCIK('1000275.0',function(data){console.log(data)});
+function query_Indexes(CIK){
+	EDGAR_query.queryCIK(CIK,function(data){
+		//DECIDE WHAT O DO WITH THE RETURNED DATA
+		console.log(data)
+	})
+}
 
-store_Indexes(years,quarters);	
+query_Indexes('1000275.0')
